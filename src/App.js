b3cd5e7b9799b78d6b193d6f5124f99787cd4b59
Josué Garza <josuegarza.dev@gmail.com>
2023-10-19 20:06:39 -0600

    testimonial funcional

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
-import  {NavBar} from './components/NavBar';
-import  {Banner} from './components/Banner';
-import {Skills} from './components/Skills';
-import {Projects} from './components/Projects';
-import {Footer} from './components/Footer';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Contact } from './components/Contact';
+import { Testimonials } from './components/Testimonials';
+import { AboutUs } from './components/Aboutus';
+import { Timeline } from './components/Timeline';
+
+
 
 
 function App() {
   return (
     <div className="App">
-        <NavBar/>
-        <Banner/>
-         <Skills/>
-        <Projects/> 
-        <Contact/>
-        <Footer/>
+      <NavBar />
+      <Banner />
+      <Timeline />
+      {/* <AboutUs /> */}
+      <Skills />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Aboutus.js
@@ -0,0 +1,102 @@
+import { Container, Row, Col } from "react-bootstrap";
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
+import proj1 from '../assets/img/servis.png';
+import proj2 from '../assets/img/tono.png';
+import proj3 from '../assets/img/roomreserve.png';
+
+export const AboutUs = () => {
+
+  const testimonials = [
+    {
+      title: "Servismedicamp",
+      subtitle: "Servismedicamp",
+      imgUrl: proj1,
+    },
+
+    {
+      title: "Motivacion secu",
+      imgUrl: proj2,
+    },
+
+    {
+      title: "Room reserve",
+      imgUrl: proj3,
+    },
+  ];
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: {
+        max: 4000,
+        min: 3000
+      },
+      items: 3
+    },
+
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024
+      },
+      items: 1
+    },
+
+
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464
+      },
+      items: 1
+    },
+
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0
+      },
+      items: 1
+    },
+
+  };
+
+  return (
+    <section className="testimonial" id="testimonials">
+      <Container>
+        <Row>
+          <Col>
+            <div className="testimonial-bx">
+
+              <div className="container" id="testimonial-bx-text">
+                <h2 style={{ color: '#fff' }}>
+                  Testimonials
+                </h2>
+                <p style={{ color: '#fff' }}>People that trust in our work</p>
+
+              </div>
+
+              <Carousel responsive={responsive} infinite={true} className="testimonial-slider">
+
+                {
+                  testimonials.map((testimonial, index) => {
+                    return (
+
+                      <div className="item"
+                        key={index}>
+                        <img src={testimonial.imgUrl} alt="anImage" />
+                        <h5>{testimonial.title}</h5>
+                        <p>{testimonial.subtitle}</p>
+
+                      </div>
+                    )
+                  })
+                }
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
